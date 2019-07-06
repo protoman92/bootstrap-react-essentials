6ce7b13b4c55507cfbe8f98f3d4e51341c970131
Hai Pham <swiften.svc@gmail.com>
2019-07-06 12:18:22 +0800

    feat(bootstrap): add global type definitions

---
+++ src/global.d.ts
@@ -0,0 +1,54 @@
+import { AxiosRequestConfig } from "axios";
+import { Action } from "redux";
+
+declare global {
+  type OmitKeys<T, K extends keyof T> = import("ts-essentials").Omit<T, K>;
+  type DeepPartial<T> = import("ts-essentials").DeepPartial<T>;
+  type DeepRequired<T> = import("ts-essentials").DeepRequired<T>;
+  type DeepReadonly<T> = import("ts-essentials").DeepReadonly<T>;
+  type DeepWriteable<T> = import("ts-essentials").DeepWritable<T>;
+
+  /** Common Redux state type that contains some basic properties. */
+  interface ReduxState {
+    readonly repository: Repository;
+  }
+
+  /** Common Redux action type that contains a payload. */
+  interface ReduxAction<Payload> extends Action<string> {
+    payload: Payload;
+  }
+
+  namespace HTTPClient {
+    type Config = Omit<AxiosRequestConfig, "baseURL">;
+  }
+
+  /** Standard HTTP client that can perform API requests. */
+  interface HTTPClient {
+    get<T>(url: string, c?: HTTPClient.Config): Promise<T>;
+    post<T>(url: string, body: unknown, c?: HTTPClient.Config): Promise<T>;
+    patch<T>(url: string, body: unknown, c?: HTTPClient.Config): Promise<T>;
+  }
+
+  /**
+   * Treat client and server as if originating from the same domain, and
+   * whatever URL the client is at, the server has the corresponding URL that
+   * contains the data.
+   *
+   * e.g.
+   * client -> https://localhost:8000/users/
+   * server -> https://localhost:8000/users/1
+   */
+  interface RelativeHTTPClient extends HTTPClient {}
+
+  namespace Repository {
+    interface URLDataSync {
+      get<T>(): Promise<T>;
+      update<T>(newData: T): Promise<T>;
+    }
+  }
+
+  /** Common repository that contains some default repositories. */
+  interface Repository {
+    readonly urlDataSync: Repository.URLDataSync;
+  }
+}
